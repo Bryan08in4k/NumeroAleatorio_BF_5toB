f8fc1282f08545eae79b940ebcad6ebef64b91c9
5to Bachillerato B <5to Bachillerato B@DESKTOP-FF54FBD>
2025-04-08 14:04:50 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace NumeroAleatorio_BF_5toB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.bttn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¡Adivina el juego!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Intentos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(119, 48);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 29);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(119, 103);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 29);
+            this.txt2.TabIndex = 4;
+            // 
+            // bttn1
+            // 
+            this.bttn1.Location = new System.Drawing.Point(44, 159);
+            this.bttn1.Name = "bttn1";
+            this.bttn1.Size = new System.Drawing.Size(180, 49);
+            this.bttn1.TabIndex = 5;
+            this.bttn1.Text = "Adivina";
+            this.bttn1.UseVisualStyleBackColor = true;
+            this.bttn1.Click += new System.EventHandler(this.bttn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 231);
+            this.Controls.Add(this.bttn1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button bttn1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumeroAleatorio_BF_5toB
+{
+    public partial class Form1 : Form
+    {
+        int intentos = 0; // Inicializar la variable
+        Random rand = new Random(); // Crea objeto que genere el número aleatorio 
+        int aleatorio = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            intentos = 5;
+            txt1.Text = intentos.ToString();
+            aleatorio = rand.Next(1, 100); // aleatorio = número entre 1 a 100 
+            MessageBox.Show($"{aleatorio}");
+        }
+
+        private void button1_click(object sender, EventArgs e)
+        {
+        }
+
+        private void bttn1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txt2.Text, out int num))
+            {
+                if (num == aleatorio)
+                {
+                    MessageBox.Show($"Ganó el juego. El número es {aleatorio}");
+                    DialogResult resultado = MessageBox.Show("¿Quieres volver a jugar?", "Resultado", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (resultado == DialogResult.No)
+                    {
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        intentos = 5;
+                        txt1.Text = intentos.ToString();
+                        aleatorio = rand.Next(1, 100);
+                    }
+
+                }
+                if (num > aleatorio)
+                {
+                    MessageBox.Show($"{num} es mayor al número aleatorio");
+                    intentos = intentos - 1;
+                    txt1.Text = intentos.ToString();
+                }
+                if (num < aleatorio)
+                {
+                    MessageBox.Show($"{num} es menor al número aleatorio");
+                    intentos--; // Es lo mismo que intentos = intentos - 1; 
+                    txt1.Text = intentos.ToString();
+                }
+                if (intentos == 0)
+                {
+                    DialogResult resultadp = MessageBox.Show("Se acabaron los intentos :c ¿Quieres volver a intenarlo","Game over",MessageBoxButtons.YesNo,MessageBoxIcon.Stop);
+                    if (resultado == DialogResult.No) 
+                    {
+                        MessageBox.Show("Suerte para la proxima");
+                        Application.Exit();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número valido");
+            }
+
+        }
+    }
+}
